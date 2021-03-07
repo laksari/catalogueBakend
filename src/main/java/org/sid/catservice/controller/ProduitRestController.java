abf0b0517e30db8f97c86d95e9011ab25a26b2b1
laksari <36415110+laksari@users.noreply.github.com>
2021-03-07 17:06:55 +0100

    Update ProduitRestController.java
    modification 10

--- src/main/java/org/sid/catservice/controller/ProduitRestController.java
+++ src/main/java/org/sid/catservice/controller/ProduitRestController.java
@@ -25,7 +25,7 @@ public class ProduitRestController {
 
 	@Autowired
 	ProduitRepository produitDao;
-	//ttt55556666
+	//ttt55556666444
 	
 	@GetMapping("/listeProduits")
 	public Page<Produit> listeProduit(Pageable pageable){
